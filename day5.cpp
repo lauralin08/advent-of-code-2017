747efd1a9e88c81f5b3709b738926a2e430acb19
lauralin08 <lauralin08@gmail.com>
2017-12-15 21:04:07 -0800

    completed day 5 challenge

--- day5.cpp
+++ day5.cpp
@@ -6,6 +6,8 @@ using std::cout;
 using std::vector;
 using std::ifstream;
 
+// count steps required to hit index beyond list
+// each time an index is "hit" the jump value increases
 int stepsToExit()
 {
   int steps = 0, index = 0;
@@ -19,17 +21,52 @@ int stepsToExit()
 
     while (inputFile >> jump)
     {
-      cout << jump << " ";
       jumpList.push_back(jump);
     }
 
-    cout << endl;
+    while (index < jumpList.size())
+    {
+      int temp = jumpList[index];
+      jumpList[index]++;
+      index += temp;
+      steps++;
+    }
+  }
+
+  inputFile.close();
+
+  return steps;
+}
+
+// count steps but if jump >= 3, decrease instead of increase
+int stepsToExit2()
+{
+  int steps = 0, index = 0;
+  ifstream inputFile;
+
+  inputFile.open("./day5Input.txt");
+  if (!inputFile.fail())
+  {
+    int jump;
+    vector<int> jumpList;
+
+    while (inputFile >> jump)
+    {
+      jumpList.push_back(jump);
+    }
 
     while (index < jumpList.size())
     {
-      cout << index << " ";
-      cout << jumpList[index] << " ";
-      index += jumpList[index];
+      int temp = jumpList[index];
+      if (temp >= 3)
+      {
+        jumpList[index]--;
+      }
+      else
+      {
+        jumpList[index]++;
+      }
+      index += temp;
       steps++;
     }
   }
@@ -43,7 +80,7 @@ int main()
 {
   cout << stepsToExit() << endl;
 
-  // cout << << endl;
+  cout << stepsToExit2() << endl;
 
   return 0;
 }
