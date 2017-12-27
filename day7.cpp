caed67d09f84ce42c9ffb7d49bccbb0a2d94547f
lauralin08 <lauralin08@gmail.com>
2017-12-27 15:29:49 -0800

    started day 7 part 2 challenge

--- day7.cpp
+++ day7.cpp
@@ -2,13 +2,13 @@
 #include <fstream>
 #include <string>
 #include <cstring>
-// #include <cstdlib>
 #include <vector>
 using std::ifstream;
 using std::string;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::to_string;
 
 string getTowerBottom()
 {
@@ -76,8 +76,6 @@ string getTowerBottom()
         }
       }
     }
-
-    cout << bottoms.size() << endl;
     bottom = bottoms[0];
   }
 
@@ -88,9 +86,196 @@ string getTowerBottom()
   return bottom;
 }
 
+int getCorrectWeight()
+{
+  class ProgramList
+  {
+  protected:
+    struct ListNode
+    {
+      string program;
+      int weight;
+      ListNode *next;
+      ListNode (string p, int w, ListNode *node = 0)
+      {
+        program = p;
+        weight = w;
+        next = node;
+      }
+    };
+    ListNode *head;
+  public:
+    void add(string p, int w)
+    {
+      if (head == 0)
+      {
+        head = new ListNode(p, w);
+      }
+      else
+      {
+        ListNode *nodePtr = head;
+        while (nodePtr->next != 0)
+        {
+          nodePtr = nodePtr->next;
+        }
+        nodePtr->next = new ListNode(p, w);
+      }
+    }
+    void moveDuplicates()
+    {
+      ListNode *nodePtr = head;
+      while (nodePtr != 0)
+      {
+        if (nodePtr->weight == 0)
+        {
+          ListNode *movePtr = head;
+          while (movePtr != 0)
+          {
+            if (movePtr->program == nodePtr->program && movePtr->weight != 0)
+            {
+              nodePtr->weight = movePtr->weight;
+              ListNode *garbage = movePtr;
+              movePtr = movePtr->next;
+              delete garbage;
+              garbage = 0;
+              movePtr = 0;
+            }
+          }
+        }
+        nodePtr = nodePtr->next;
+      }
+    }
+    // int positionOf(string p)
+    // {
+    //   int index = 0;
+    //   ListNode *nodePtr = head;
+    //   while (nodePtr != 0)
+    //   {
+    //     if (nodePtr->program == p)
+    //     {
+    //       return index;
+    //     }
+    //     else
+    //     {
+    //       index++;
+    //       nodePtr = nodePtr->next;
+    //     }
+    //   }
+    //   return -1;
+    // }
+    // bool setNodeVal(int i, string p, int w)
+    // {
+    //   int counter = i - 1;
+    //   ListNode *nodePtr = head;
+    //   while (counter >= 0)
+    //   {
+    //     if (nodePtr == 0)
+    //     {
+    //       return false;
+    //     }
+    //     else
+    //     {
+    //       counter--;
+    //       nodePtr = nodePtr->next;
+    //     }
+    //   }
+    //   nodePtr->program = p;
+    //   nodePtr->weight = w;
+    //   return true;
+    // }
+    vector<string> getAsVector()
+    {
+      vector<string> programs;
+      ListNode *nodePtr = head;
+      while (nodePtr != 0)
+      {
+        programs.push_back(nodePtr->program + ": " + to_string(nodePtr->weight));
+        nodePtr = nodePtr->next;
+      }
+      return programs;
+    }
+    ProgramList()
+    {
+      head = 0;
+    }
+    ~ProgramList()
+    {
+      ListNode *nodePtr = head;
+      while (nodePtr != 0)
+      {
+        ListNode *garbage = nodePtr;
+        nodePtr = nodePtr->next;
+        delete garbage;
+      }
+    }
+  };
+
+  int correctWeight;
+  ifstream inputFile;
+  inputFile.open("./day7Input.txt");
+  if (!inputFile.fail())
+  {
+    string row;
+    ProgramList programList;
+    while (getline(inputFile, row))
+    {
+      char* input = new char[row.size() + 1];
+      for (int i = 0; i < row.size(); i++)
+      {
+        if (row[i] == ',' || row[i] == '(' || row[i] == ')')
+        {
+          input[i] = ' ';
+        }
+        else
+        {
+          input[i] = row[i];
+        }
+      }
+      input[row.size()] = '\0';
+
+      vector<char*> temp;
+      char* token = strtok(input, " ");
+      while (token != 0)
+      {
+        temp.push_back(token);
+        token = strtok(0, " ");
+      }
+
+      delete []token;
+      token = 0;
+
+      programList.add(temp[0], atoi(temp[1]));
+      for (int i = 3; i < temp.size(); i++)
+      {
+        programList.add(temp[i], 0);
+      }
+
+      delete []input;
+      input = 0;
+    }
+    
+    programList.moveDuplicates();
+
+    vector<string> programs = programList.getAsVector();
+    for (int i = 0; i < programs.size(); i++)
+    {
+      cout << programs[i] << endl;
+    }
+  }
+
+
+  inputFile.close();
+
+  cout << correctWeight << endl;
+
+  return correctWeight;
+}
+
 int main()
 {
   getTowerBottom();
+
+  getCorrectWeight();
 
   return 0;
 }
